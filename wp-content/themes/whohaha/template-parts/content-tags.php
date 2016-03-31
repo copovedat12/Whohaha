a8f8f6550817f528e47f76cf8aa2a8babc7c8049
Tyler Patton <jamestylerpatton@gmail.com>
2016-03-31 10:19:02 -0700

    Fix video gallery layout on tag pages
    Also fix menu button hover background-color in header

--- wp-content/themes/whohaha/template-parts/content-tags.php
+++ wp-content/themes/whohaha/template-parts/content-tags.php
@@ -37,10 +37,31 @@
 				<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 			</header><!-- .entry-header -->
 
-			<?php if ( get_post_format() == 'video' && get_field('video_embed_code') ): ?>
-				<div class="video-embed">
-					<?php echo get_post_meta($post->ID, 'video_embed_code', true); ?>
+			<?php if ( get_field('video_embed_code') ):
+				if(have_rows('add_more_videos')):
+				?>
+
+				<div class="video-expanded-gallery">
+					<div>
+						<div class="video-embed<?php if(get_field('video_type') === 'instagram'): ?>-insta<?php endif; ?> <?php the_field('video_type'); ?>">
+							<?php echo get_field('video_embed_code'); ?>
+						</div>
+					</div>
+					<?php while (have_rows('add_more_videos')): the_row(); ?>
+						<div>
+							<div class="video-embed<?php if(get_field('video_type') === 'instagram'): ?>-insta<?php endif; ?> <?php the_sub_field('extra_video_type') ?>">
+								<?php the_sub_field('extra_video_code') ?>
+							</div>
+						</div>
+					<?php endwhile; ?>
 				</div>
+
+				<?php else: ?>
+
+				<div class="video-embed">
+					<?php echo get_field('video_embed_code'); ?>
+				</div>
+				<?php endif;?>
 			<?php else: ?>
 				<div class="post-featured-image">
 					<?php the_post_thumbnail('full') ?>
