38fbe85a35d901f52f775e54d486d7fffee7ff4d
Tyler Patton <jamestylerpatton@gmail.com>
2016-04-01 16:12:10 -0700

    Change really important questions link
    Make archive-footer show WhoHaha and Chill section

--- wp-content/themes/whohaha/template-parts/content-archive-footer.php
+++ wp-content/themes/whohaha/template-parts/content-archive-footer.php
@@ -1,24 +1,61 @@
 <div class="row">
 	<div class="col-md-12">
 		<header class="top-header">
-			<span>More HAHAs</span>
+			<span>WhoHaha &amp; Chill</span>
 		</header>
-		<div class="row">
-			<?php
-			// $pageid = get_the_ID();
-			global $do_not_duplicate;
-			$args = array(
-				'orderby'        => 'rand',
-				'posts_per_page' => '3',
-				'post__not_in' => $do_not_duplicate
-			);
-			$post_loop = new WP_Query($args);
-			while ( $post_loop->have_posts() ) : $post_loop->the_post(); 
-			// $loop_index++;
-			$do_not_duplicate[] = $post->ID;
-			?>
-			<?php get_template_part( 'template-parts/postpage', 'more-to-love' ); ?>
-			<?php endwhile; wp_reset_postdata(); ?>
+		<div class="col-md-4 col-sm-6">
+			<article class="post bottom-rest">
+				<div class="entry-image">
+					<a href="/ask-a-badass/">
+						<img src="<?php echo get_template_directory_uri(); ?>/resources/images/post-footer/askabadass.jpg" alt="Ask A Badass">
+						<div class="play-btn">
+							<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:a="http://ns.adobe.com/AdobeSVGViewerExtensions/3.0/" x="0px" y="0px" width="100px" height="100px" viewBox="0 0 213.7 213.7" enable-background="new 0 0 213.7 213.7" xml:space="preserve">
+								<polygon class="triangle" id="XMLID_18_" fill="none" stroke-width="7" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" points="73.5,62.5 148.5,105.8 73.5,149.1 "></polygon>
+								<circle class="circle" id="XMLID_17_" fill="none" stroke-width="7" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" cx="106.8" cy="106.8" r="103.3"></circle>
+							</svg>
+						</div>
+					</a>
+					<div class="entry-content">
+						<h2 class="entry-title"><a href="/ask-a-badass/" rel="bookmark">Ask A Badass</a></h2>
+					</div><!-- .entry-content -->
+				</div>
+			</article><!-- #post-## -->
+		</div>
+		<div class="col-md-4 col-sm-6">
+			<article class="post bottom-rest">
+				<div class="entry-image">
+					<a href="/really-important-questions-really-important-answers/">
+						<img src="<?php echo get_template_directory_uri(); ?>/resources/images/post-footer/reallyimportantquestions.jpg" alt="Really Important Questions">
+						<div class="play-btn">
+							<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:a="http://ns.adobe.com/AdobeSVGViewerExtensions/3.0/" x="0px" y="0px" width="100px" height="100px" viewBox="0 0 213.7 213.7" enable-background="new 0 0 213.7 213.7" xml:space="preserve">
+								<polygon class="triangle" id="XMLID_18_" fill="none" stroke-width="7" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" points="73.5,62.5 148.5,105.8 73.5,149.1 "></polygon>
+								<circle class="circle" id="XMLID_17_" fill="none" stroke-width="7" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" cx="106.8" cy="106.8" r="103.3"></circle>
+							</svg>
+						</div>
+					</a>
+					<div class="entry-content">
+						<h2 class="entry-title"><a href="/really-important-questions-really-important-answers/" rel="bookmark">Really Important Questions</a></h2>
+					</div><!-- .entry-content -->
+				</div>
+			</article><!-- #post-## -->
+		</div>
+		<div class="col-md-4 col-sm-6">
+			<article class="post bottom-rest">
+				<div class="entry-image">
+					<a href="/tv/">
+						<img src="<?php echo get_template_directory_uri(); ?>/resources/images/post-footer/whohahaTV.jpg" alt="WhoHaha TV">
+						<div class="play-btn">
+							<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:a="http://ns.adobe.com/AdobeSVGViewerExtensions/3.0/" x="0px" y="0px" width="100px" height="100px" viewBox="0 0 213.7 213.7" enable-background="new 0 0 213.7 213.7" xml:space="preserve">
+								<polygon class="triangle" id="XMLID_18_" fill="none" stroke-width="7" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" points="73.5,62.5 148.5,105.8 73.5,149.1 "></polygon>
+								<circle class="circle" id="XMLID_17_" fill="none" stroke-width="7" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" cx="106.8" cy="106.8" r="103.3"></circle>
+							</svg>
+						</div>
+					</a>
+					<div class="entry-content">
+						<h2 class="entry-title"><a href="/tv/" rel="bookmark">WhoHaha TV</a></h2>
+					</div><!-- .entry-content -->
+				</div>
+			</article><!-- #post-## -->
 		</div>
 	</div>
 </div>
