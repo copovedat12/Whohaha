a58b7e1923e4f8e91aa316f0a6b7ebc6d88586bd
Tyler Patton <jamestylerpatton@gmail.com>
2016-04-01 16:02:55 -0700

    Change Like page to WhoHaha FB page instead of site

--- wp-content/themes/whohaha/template-parts/loop-breaks/break-part-3.php
+++ wp-content/themes/whohaha/template-parts/loop-breaks/break-part-3.php
@@ -2,7 +2,7 @@
 	<div class="col-md-6">
 		<div class="break-social fb">
 			<img src="<?php echo get_template_directory_uri().'/resources/images/facebook-break-min.jpg' ?>" alt="Connect on Facebook">
-			<div class="fb-like" data-href="http://whohaha.com/" data-layout="box_count" data-action="like" data-show-faces="false" data-share="false"></div>
+			<div class="fb-like" data-href="https://www.facebook.com/whohaha/" data-layout="box_count" data-action="like" data-show-faces="false" data-share="false"></div>
 		</div>
 	</div>
 	<div class="col-md-6">
