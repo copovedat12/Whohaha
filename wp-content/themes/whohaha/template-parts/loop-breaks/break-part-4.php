eff265996be2274eaee7614c702f1e4dcbfacde7
Tyler Patton <jamestylerpatton@gmail.com>
2016-04-01 15:32:07 -0700

    Change More Hahas section to show playlist pages
    Remove ask-a-girl banner from loop break

--- wp-content/themes/whohaha/template-parts/loop-breaks/break-part-4.php
+++ wp-content/themes/whohaha/template-parts/loop-breaks/break-part-4.php
@@ -1,8 +1,5 @@
 <div class="row">
-	<div class="col-md-6 break-banner-first">
-		<a href="/ask-a-girl-episode-1-will-american-girls-dig-my-cute-british-accent/"><img src="<?php echo get_template_directory_uri(); ?>/resources/images/breaks/ask-a-girl-banner.png" alt="Ask A Girl"></a>
-	</div>
-	<div class="col-md-6 break-banner-second">
+	<div class="col-md-12">
 		<a href="/tag/quizzes/"><img src="<?php echo get_template_directory_uri(); ?>/resources/images/breaks/WHH_Quizzes_banner.png" alt="Take our quizzes"></a>
 	</div>
 </div>
