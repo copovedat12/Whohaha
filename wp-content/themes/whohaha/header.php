49a2301dacd3121cca2815558e03e2e3ecfbb8b6
Tyler Patton <jamestylerpatton@gmail.com>
2016-04-01 18:15:26 -0700

    Add Google Analytics to header

--- wp-content/themes/whohaha/header.php
+++ wp-content/themes/whohaha/header.php
@@ -53,6 +53,16 @@
 <meta name="google-site-verification" content="YLaxceYycBEBXkM_yGMEhKD0IioeOHSM_EI-nnH81pE" />
 <meta name="google-site-verification" content="civVdocHhY9An9L-iLyjzsphwp45HNfxxth8V_QsOH8" />
 
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-13198218-54', 'auto');
+  ga('send', 'pageview');
+</script>
+
 <link href='https://fonts.googleapis.com/css?family=Oswald:300|Lato:900,400,400italic,300|Open+Sans+Condensed:300' rel='stylesheet' type='text/css'>
 
 <?php wp_head(); ?>
