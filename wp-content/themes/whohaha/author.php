48def58d919f0fe8dec223867db5e58415fbcff1
Tyler Patton <jamestylerpatton@gmail.com>
2016-04-28 17:52:35 -0700

    Style author template
    This affects the "ladies" page

--- wp-content/themes/whohaha/author.php
+++ wp-content/themes/whohaha/author.php
@@ -13,38 +13,48 @@ get_header(); ?>
 		<main id="main" class="site-main container author" role="main">
 
 		<section class="author-header">
-			<?php $author_image = get_field('profile_image', 'user_'.get_the_author_id());
-			if($author_image): ?>
-			<div class="author-image">
-				<img src="<?php echo $author_image['sizes']['medium']; ?>" width="<?php echo $author_image['sizes'][ 'medium-width' ]; ?>" height="<?php echo $author_image['sizes'][ 'medium-height' ]; ?>">
-			</div>
-			<?php endif; ?>
-			<div class="author-bio">
-				<h1><?php echo get_the_author(); ?></h1>
-				<?php if(get_the_author_meta( 'description', get_the_author_id() )): ?>
-				<p><?php the_author_meta( 'description', get_the_author_id() ); ?></p>
-				<?php endif; ?>
-				<p class="social-links">
-					<?php 
-					if(get_usermeta(get_the_author_id(),'user_url')): ?>
-					<a class="socicon" href="<?php the_author_meta( 'user_url'); ?>" target="_blank"><i class="fi-web"></i></a>
-					<?php endif;
-					if(get_field('user_fb', 'user_'.get_the_author_id())): ?>
-					<a class="socicon" href="<?php the_field('user_fb', 'user_'.get_the_author_id()); ?>" target="_blank">b</a>
-					<?php endif;
-						  if(get_field('user_tw', 'user_'.get_the_author_id())): ?>
-					<a class="socicon" href="<?php the_field('user_tw', 'user_'.get_the_author_id()); ?>" target="_blank">a</a>
-					<?php endif;
-						  if(get_field('user_ig', 'user_'.get_the_author_id())): ?>
-					<a class="socicon" href="<?php the_field('user_ig', 'user_'.get_the_author_id()); ?>" target="_blank">x</a>
-					<?php endif;
-						  if(get_field('user_yt', 'user_'.get_the_author_id())): ?>
-					<a class="socicon" href="<?php the_field('user_yt', 'user_'.get_the_author_id()); ?>" target="_blank">r</a>
-					<?php endif;
-						  if(get_field('user_pin', 'user_'.get_the_author_id())): ?>
-					<a class="socicon" href="<?php the_field('user_pin', 'user_'.get_the_author_id()); ?>" target="_blank">d</a>
-					<?php endif; ?>
-				</p>
+			<div class="row">
+				<div class="col-md-8 col-md-offset-2">
+					<div class="row">
+						<div class="col-sm-4">
+							<?php $author_image = get_field('profile_image', 'user_'.get_the_author_id());
+							if($author_image): ?>
+							<div class="author-image">
+								<img src="<?php echo $author_image['sizes']['medium']; ?>" width="<?php echo $author_image['sizes'][ 'medium-width' ]; ?>" height="<?php echo $author_image['sizes'][ 'medium-height' ]; ?>">
+							</div>
+							<?php endif; ?>
+						</div>
+						<div class="col-sm-8">
+							<div class="author-bio">
+								<h1><?php echo get_the_author(); ?></h1>
+								<?php if(get_the_author_meta( 'description', get_the_author_id() )): ?>
+								<p><?php the_author_meta( 'description', get_the_author_id() ); ?></p>
+								<?php endif; ?>
+								<p class="social-links">
+									<?php 
+									if(get_usermeta(get_the_author_id(),'user_url')): ?>
+									<a class="socicon" href="<?php the_author_meta( 'user_url'); ?>" target="_blank"><i class="fi-web"></i></a>
+									<?php endif;
+									if(get_field('user_fb', 'user_'.get_the_author_id())): ?>
+									<a class="socicon" href="<?php the_field('user_fb', 'user_'.get_the_author_id()); ?>" target="_blank">b</a>
+									<?php endif;
+										  if(get_field('user_tw', 'user_'.get_the_author_id())): ?>
+									<a class="socicon" href="<?php the_field('user_tw', 'user_'.get_the_author_id()); ?>" target="_blank">a</a>
+									<?php endif;
+										  if(get_field('user_ig', 'user_'.get_the_author_id())): ?>
+									<a class="socicon" href="<?php the_field('user_ig', 'user_'.get_the_author_id()); ?>" target="_blank">x</a>
+									<?php endif;
+										  if(get_field('user_yt', 'user_'.get_the_author_id())): ?>
+									<a class="socicon" href="<?php the_field('user_yt', 'user_'.get_the_author_id()); ?>" target="_blank">r</a>
+									<?php endif;
+										  if(get_field('user_pin', 'user_'.get_the_author_id())): ?>
+									<a class="socicon" href="<?php the_field('user_pin', 'user_'.get_the_author_id()); ?>" target="_blank">d</a>
+									<?php endif; ?>
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
 		</section>
 
