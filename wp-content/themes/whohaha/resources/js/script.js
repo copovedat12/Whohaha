63808d05a9e988fdcb84065e0d465dd977c0fc12
Tyler Patton <jamestylerpatton@gmail.com>
2016-05-11 13:11:30 -0700

    Add option to show Author in post footer

--- wp-content/themes/whohaha/resources/js/script.js
+++ wp-content/themes/whohaha/resources/js/script.js
@@ -139,6 +139,38 @@
 			]
 		});
 
+		$('.in-post-carousel-with-author').slick({
+			dots: false,
+			arrows : false,
+			infinite: false,
+			speed: 300,
+			slidesToShow: 3,
+			slidesToScroll: 3,
+			responsive: [
+			  {
+				breakpoint: 1024,
+				settings: {
+				  slidesToShow: 3,
+				  slidesToScroll: 3
+				}
+			  },
+			  {
+				breakpoint: 600,
+				settings: {
+				  slidesToShow: 2,
+				  slidesToScroll: 2
+				}
+			  },
+			  {
+				breakpoint: 480,
+				settings: {
+				  slidesToShow: 1,
+				  slidesToScroll: 1
+				}
+			  }
+			]
+		});
+
 		$('.video-carousel').slick({
 			arrows : true,
 			infinite : true,
