cbb1d29e7b11363c948398e7c0cedeec0d33a41f
Tyler Patton <jamestylerpatton@gmail.com>
2016-06-22 11:00:19 -0700

    Only add youtube video if post meta exists in instant article

--- wp-content/themes/whohaha/inc/instant-articles.php
+++ wp-content/themes/whohaha/inc/instant-articles.php
@@ -12,12 +12,14 @@ use Facebook\InstantArticles\Elements\SocialEmbed;
 // } );
 
 function incl_video($ia_post) {
-    $instant_article = $ia_post->instant_article;
-    $post_id = $ia_post->get_the_id();
-    $iframe_string = get_post_meta( $post_id, 'video_embed_code', true );
-    preg_match('/src="([^"]+)"/', $iframe_string, $match);
-    $video_url = $match[1];
-    $instant_article->addChild( SocialEmbed::create()->withSource($video_url) );
+	$instant_article = $ia_post->instant_article;
+	$post_id = $ia_post->get_the_id();
+	$iframe_string = get_post_meta( $post_id, 'video_embed_code', true );
+	if($iframe_string){
+		preg_match('/src="([^"]+)"/', $iframe_string, $match);
+		$video_url = $match[1];
+		$instant_article->addChild( SocialEmbed::create()->withSource($video_url) );
+	}
 }
 
 // add_filter( 'instant_articles_parsed_document', 'incl_video', 1 );
