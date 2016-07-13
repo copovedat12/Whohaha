2f0dc995f36d73d8b2a5621b0c5365178f1af484
Tyler Patton <jamestylerpatton@gmail.com>
2016-07-13 16:17:14 -0700

    Center doubleclick ads

--- wp-content/themes/whohaha/template-parts/loop-breaks/break-part-4.php
+++ wp-content/themes/whohaha/template-parts/loop-breaks/break-part-4.php
@@ -2,7 +2,7 @@
 	<div class="col-md-12">
 		<!-- <a href="/tag/quizzes/"><img src="<?php echo get_template_directory_uri(); ?>/resources/images/breaks/WHH_Quizzes.png" alt="Take our quizzes"></a> -->
 		<!-- /4738791/WHH_Spot_2 -->
-		<div id='div-gpt-ad-1468262843164-0'>
+		<div id='div-gpt-ad-1468262843164-0' class="db-ad">
 		<script type='text/javascript'>
 		googletag.cmd.push(function() { googletag.display('div-gpt-ad-1468262843164-0'); });
 		</script>
