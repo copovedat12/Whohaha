d96b9a61ad2e64504faa957192e456624e3d9279
Tyler Patton <jamestylerpatton@gmail.com>
2016-07-13 16:07:43 -0700

    Call differnt ad div

--- wp-content/themes/whohaha/template-parts/loop-breaks/break-part-4.php
+++ wp-content/themes/whohaha/template-parts/loop-breaks/break-part-4.php
@@ -2,9 +2,9 @@
 	<div class="col-md-12">
 		<!-- <a href="/tag/quizzes/"><img src="<?php echo get_template_directory_uri(); ?>/resources/images/breaks/WHH_Quizzes.png" alt="Take our quizzes"></a> -->
 		<!-- /4738791/WHH_Spot_2 -->
-		<div id='div-gpt-ad-1468262704510-0'>
+		<div id='div-gpt-ad-1468262843164-0'>
 		<script type='text/javascript'>
-		googletag.cmd.push(function() { googletag.display('div-gpt-ad-1468262704510-0'); });
+		googletag.cmd.push(function() { googletag.display('div-gpt-ad-1468262843164-0'); });
 		</script>
 		</div>
 	</div>
