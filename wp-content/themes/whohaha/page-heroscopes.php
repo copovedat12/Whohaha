7939231a14654a4b3b855170c1ec0fd66b7133a2
Tyler Patton <jamestylerpatton@gmail.com>
2016-12-02 12:12:07 -0800

    Add template file for playlist pages
    Add heroscope page file - still being worked on
    Remove laughstagram page

---
+++ wp-content/themes/whohaha/page-heroscopes.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * The template for whohaha tv page.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package whohaha
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main container category" role="main">
+			<header class="page-header top-header">
+				<span>HEROSCOPES</span>
+			</header><!-- .page-header -->
+
+			<?php getVideoPlaylist('https://www.youtube.com/playlist?list=PLdt4fwPI6A7SqDGWYcVSUbgoir84t24Wf'); ?>
+
+			<div class="row">
+				<div class="col-md-8">
+					socials <br>
+					<?php while ( have_posts() ) : the_post(); ?>
+						<?php echo get_the_content(); ?>
+					<?php endwhile; ?>
+				</div>
+				<div class="col-md-4">
+					img
+				</div>
+			</div>
+
+		<?php
+			get_template_part( 'template-parts/content', 'archive-footer' );
+		?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php //get_sidebar(); ?>
+<?php get_footer(); ?>
---
+++ wp-content/themes/whohaha/page-playlist.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Template Name: Playlist Page
+ *
+ * @package WordPress
+ * @subpackage WhoHaha
+ * @since whohaha
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package whohaha
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main container category" role="main">
+			<header class="page-header top-header">
+				<span><?php echo get_the_title(); ?></span>
+			</header><!-- .page-header -->
+
+			<div class="whohaha-tv-intro">
+				<div class="row">
+					<div class="col-md-8 col-md-offset-2">
+						<h3 class="text-center">
+						<?php while ( have_posts() ) : the_post(); ?>
+							<?php echo get_the_content(); ?>
+						<?php endwhile; ?>
+						</h3>
+					</div>
+				</div>
+			</div>
+
+			<?php getVideoPlaylist('https://www.youtube.com/playlist?list=PLdt4fwPI6A7TCF2nXileEEdj7i8HGivGt'); ?>
+
+		<?php
+			get_template_part( 'template-parts/content', 'archive-footer' );
+		?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php //get_sidebar(); ?>
+<?php get_footer(); ?>
