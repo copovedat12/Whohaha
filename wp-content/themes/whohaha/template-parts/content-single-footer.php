7d1c41350e38427d11c32fb2100ef4179c246e2e
Tyler Patton <jamestylerpatton@gmail.com>
2017-01-18 11:16:32 -0800

    Hide More Like This and WHH and Chill footer from aha posts

--- wp-content/themes/whohaha/template-parts/content-single-footer.php
+++ wp-content/themes/whohaha/template-parts/content-single-footer.php
@@ -1,5 +1,7 @@
 <footer class="post-footer">
-	<?php
+	<?php if (!has_tag('aha')): ?>
+
+		<?php
 		if(get_the_id() !== 3215 && get_the_id() !== 3208):
 
 		global $do_not_duplicate;
@@ -21,34 +23,36 @@
 		);
 		$post_loop = new WP_Query($args);
 		if($post_loop->have_posts() && $post_loop->found_posts > 1 && $available_tags):
-	?>
-	<div class="row">
-		<div class="col-md-12">
-			<header class="top-header">
-				<span>More Like This</span>
-			</header>
-			<div class="row">
-				<?php
-				while ( $post_loop->have_posts() ) : $post_loop->the_post(); 
-				// $loop_index++;
-				$do_not_duplicate[] = $post_loop->post->ID;
-				?>
-				<?php get_template_part( 'template-parts/content-displayposts', 'large' ); ?>
-				<?php endwhile; wp_reset_postdata(); ?>
+		?>
+		<div class="row">
+			<div class="col-md-12">
+				<header class="top-header">
+					<span>More Like This</span>
+				</header>
+				<div class="row">
+					<?php
+					while ( $post_loop->have_posts() ) : $post_loop->the_post(); 
+					// $loop_index++;
+					$do_not_duplicate[] = $post_loop->post->ID;
+					?>
+					<?php get_template_part( 'template-parts/content-displayposts', 'large' ); ?>
+					<?php endwhile; wp_reset_postdata(); ?>
+				</div>
 			</div>
 		</div>
-	</div>
-	<?php endif; endif; ?>
+		<?php endif; endif; ?>
 
-	<section class="whh-playlists">
-		<header class="top-header home-author-header">
-			<span>WhoHaha &amp; Chill</span>
-		</header>
-		<?php
-		whh_render_all_series();
-		whh_render_single_series([ 'limit' => 1, 'shuffle' => true ]);
-		?>
-	</section>
+		<section class="whh-playlists">
+			<header class="top-header home-author-header">
+				<span>WhoHaha &amp; Chill</span>
+			</header>
+			<?php
+			whh_render_all_series();
+			whh_render_single_series([ 'limit' => 1, 'shuffle' => true ]);
+			?>
+		</section>
+
+	<?php endif; // !has_tag('aha') ?>
 
 	<div class="row">
 		<div class="col-md-12">
