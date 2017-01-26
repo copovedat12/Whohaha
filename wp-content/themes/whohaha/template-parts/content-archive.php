592743d32aff8d90e5a25ce73f7ef6f0301e9a73
Tyler Patton <jamestylerpatton@gmail.com>
2017-01-26 14:00:00 -0800

    Show heroscopes on Alex Lynn Ward's page

--- wp-content/themes/whohaha/template-parts/content-archive.php
+++ wp-content/themes/whohaha/template-parts/content-archive.php
@@ -10,9 +10,33 @@ if(have_posts() && !is_paged() ):
 		);
 		if(is_category()) $args['cat'] = get_query_var('cat');
 		if(is_author()){
-			// $args['post_status'] = 'publish';
-			// $args['post_type'] = 'post';
 			$args['author_name'] = get_the_author_meta( 'user_nicename', $author->ID );
+
+			/**
+			 * If author is Alex Lynn Ward, show heroscopes as post
+			 */
+			if (get_the_author_meta( 'user_nicename', $author->ID ) === 'alex-lynn-ward') {
+				$args['posts_per_page'] = 1;
+				$heroscopes = get_terms(array('taxonomy' => 'playlists', 'slug' => 'heroscopes'));
+				$heroscopes = $heroscopes[0];
+				$playlist_feat_img = get_field('featured_image', $heroscopes);
+				?>
+				<div class="col-sm-6">
+					<article class="post top-two format-video">
+						<div class="entry-image">
+							<a href="<?php echo get_term_link($heroscopes); ?>">
+								<img src="<?php echo $playlist_feat_img['sizes']['home-posts-lg']; ?>" alt="<?php echo $playlist_feat_img['alt']; ?>" width="<?php echo $playlist_feat_img['sizes']['home-posts-lg-width']; ?>" height="<?php echo $playlist_feat_img['sizes']['home-posts-lg-height']; ?>">
+								<?php get_template_part('template-parts/play-button'); ?>
+							</a>
+
+							<div class="entry-content">
+								<h2 class="entry-title"><a href="<?php echo get_term_link($heroscopes); ?>" rel="bookmark"><?php echo $heroscopes->name; ?></a></h2>
+							</div><!-- .entry-content -->
+						</div>
+					</article><!-- #post-## -->
+				</div>
+				<?php
+			}
 		}
 		if(is_tag()) $args['tag_id'] = get_query_var('tag_id');
 
