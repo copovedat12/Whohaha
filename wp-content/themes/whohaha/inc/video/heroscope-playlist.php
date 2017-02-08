c3aeb5e190d970e2f1cd544b47da28b39498f2d0
Tyler Patton <jamestylerpatton@gmail.com>
2017-02-07 16:19:23 -0800

    Use new version for heroscope script

--- wp-content/themes/whohaha/inc/video/heroscope-playlist.php
+++ wp-content/themes/whohaha/inc/video/heroscope-playlist.php
@@ -8,7 +8,7 @@ function get_heroscope_plist(){
 	<div id="player" class="video-iframe"></div>
 	<?php
 	wp_enqueue_script( 'dm_api', 'https://api.dmcdn.net/all.js', false, false, false );
-	wp_enqueue_script( 'heroscope_player', get_template_directory_uri() . '/resources/js/heroscopes.js', array('jquery'), '1.0.1', true );
+	wp_enqueue_script( 'heroscope_player', get_template_directory_uri() . '/resources/js/heroscopes.js', array('jquery'), '2.0.0', true );
 }
 
 function heroscopes_page_urls(){
