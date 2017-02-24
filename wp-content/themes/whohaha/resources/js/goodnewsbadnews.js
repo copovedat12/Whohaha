30a200d1c0af7b503efc06ee7fbc32f45daa711f
Tyler Patton <jamestylerpatton@gmail.com>
2017-02-24 12:12:52 -0800

    Refactor good news bad news javascript

--- wp-content/themes/whohaha/resources/js/goodnewsbadnews.js
+++ wp-content/themes/whohaha/resources/js/goodnewsbadnews.js
@@ -1,55 +1,64 @@
 var gnbnPlaylists = (function($){
-	var player = [],
-		playerIndex = [],
-		playerTypes = ['good', 'bad'],
-		playerIds = [],
-		videoAutoplay = [false, false],
-		firstIds = [];
-
+	var player = {},
+		playerObjects = {
+			good : {
+				firstId : null,
+				playerIds : [],
+				autoplay : false
+			},
+			bad : {
+				firstId : null,
+				playerIds : [],
+				autoplay : false
+			}
+		};
 	if ( typeof(GNBN_PLIST_ARR) !== 'undefined' ) {
-		playerIds[0] = GNBN_PLIST_ARR.goodNewsUrls;
-		playerIds[1] = GNBN_PLIST_ARR.badNewsUrls;
-		firstIds = [playerIds[0][0], playerIds[1][0]];
+		playerObjects.good.playerIds = GNBN_PLIST_ARR.goodNewsUrls;
+		playerObjects.good.firstId = playerObjects.good.playerIds[0];
+		playerObjects.bad.playerIds = GNBN_PLIST_ARR.badNewsUrls;
+		playerObjects.bad.firstId = playerObjects.bad.playerIds[0];
 	}
+
 	if ( window.location.hash.length > 0 ) {
 		var hashId = window.location.hash.replace('#','');
-		for (id in playerIds) {
-			if ( playerIds[id].indexOf(hashId) > -1 ) {
-				firstIds[id] = hashId;
-				videoAutoplay[id] = true;
+		for (id in playerObjects) {
+			if ( playerObjects[id].playerIds.indexOf(hashId) > -1 ) {
+				playerObjects[id].firstId = hashId;
+				playerObjects[id].autoplay = true;
 			}
 		}
 	}
 
-	for (var i = 0; i < playerTypes.length; i++) {
-		var playerType = playerTypes[i]; // 'good' or 'bad'
-		var currentVideo = firstIds[i];
-		playerIndex[i] = i;
+	for (i in playerObjects) {
+		var playerType = i; // 'good' or 'bad'
+		var currentVideo = playerObjects[i].firstId;
 
-		player[i] = DM.player(document.getElementById("player_"+playerType), {
+		player[i] = DM.player(document.getElementById("player_"+i), {
 			video: currentVideo,
 			width: "100%",
 			height: "100%",
 			params: {
-				autoplay: videoAutoplay[i],
+				autoplay: playerObjects[i].autoplay,
 				mute: false
 			}
 		});
-		player[i].index = i;
+		player[i].type = i;
 		player[i].currentVideoId = currentVideo;
 		player[i].addEventListener('apiready', function(){
 			$(this).css('opacity', 1);
 		});
 		player[i].addEventListener('end', function(e){
-			var thisPlayerIds = playerIds[this.index];
-			var idIndex = thisPlayerIds.indexOf(this.currentVideoId),
-				nextVid;
-			if ( playerIds[this.index][idIndex+1] !== 'undefined' && idIndex > -1) {
-				nextVid = playerIds[this.index][idIndex+1];
+			var thisPlayer = playerObjects[this.type],
+				thisCurrent = this.currentVideoId;
+
+			var currentIndex = thisPlayer.playerIds.indexOf(thisCurrent);
+
+			if ( typeof( thisPlayer.playerIds[currentIndex+1] ) !== 'undefined' ) {
+				nextVid = thisPlayer.playerIds[currentIndex+1];
 			} else {
-				nextVid = playerIds[this.index][0];
+				nextVid = thisPlayer.playerIds[0];
 			}
-			player[this.index].currentVideoId = nextVid;
+			player[this.type].currentVideoId = nextVid;
 			this.load(nextVid, {
 				autoplay : true
 			});
